d58db9b2f25bbcc95aefd44e6dd721a95c763925
Chaman Betrabet <betrabet.c@northeastern.edu>
2024-09-23 15:42:20 -0400

    Intialized modules and used ORM to create user table

---
+++ models/user.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint      `gorm:"primaryKey;autoIncrement" json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `gorm:"uniqueIndex" json:"email"`
+	DOB       time.Time `json:"dob"`
+	Age       int       `json:"age" gorm:"-"`
+}
+
+func (u *User) BeforeSave(tx *gorm.DB) (err error) {
+	u.Age = CalculateAge(u.DOB)
+	return
+}
+
+func CalculateAge(dob time.Time) int {
+	today := time.Now()
+	age := today.Year() - dob.Year()
+	if today.YearDay() < dob.YearDay() {
+		age--
+	}
+	return age
+}
