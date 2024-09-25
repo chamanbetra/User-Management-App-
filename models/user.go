7202e66bf1cfc9b0ba329268f10138c0032d98dc
Chaman Betrabet <betrabet.c@northeastern.edu>
2024-09-25 11:11:27 -0400

    Changed age datatype from Time to String

--- models/user.go
+++ models/user.go
@@ -1,18 +1,19 @@
 package models
 
 import (
+	"fmt"
 	"time"
 
 	"gorm.io/gorm"
 )
 
 type User struct {
-	ID        uint      `gorm:"primaryKey;autoIncrement" json:"id"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Email     string    `gorm:"type:varchar(255);uniqueIndex" json:"email"`
-	DOB       time.Time `json:"dob"`
-	Age       int       `json:"age" gorm:"-"`
+	ID        uint   `gorm:"primaryKey;autoIncrement" json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `gorm:"type:varchar(255);uniqueIndex" json:"email"`
+	DOB       string `json:"dob"`
+	Age       int    `json:"age"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
@@ -22,7 +23,13 @@ func (u *User) BeforeSave(tx *gorm.DB) (err error) {
 	return
 }
 
-func CalculateAge(dob time.Time) int {
+func CalculateAge(dobStr string) int {
+	dob, err := time.Parse("2006-01-02", dobStr) // Change format according to the input format
+	if err != nil {
+		fmt.Println("Error parsing DOB:", err)
+		return 0 // Return 0 or handle the error as per your logic
+	}
+
 	today := time.Now()
 	age := today.Year() - dob.Year()
 	if today.YearDay() < dob.YearDay() {
