0cddf1639526931aa2b188a4c773ad375bf2a500
Chaman Betrabet <betrabet.c@northeastern.edu>
2024-09-27 16:54:31 -0400

    Modified dummy unit and integration tests

--- integration/integration_tests.go
+++ integration/integration_tests.go
@@ -6,16 +6,20 @@ import (
 
 func TestIntegrationDummyCreateUser(t *testing.T) {
 	// This integration test will always pass
+	t.Log("Dummy test passed")
 }
 
 func TestIntegrationDummyGetUserByEmail(t *testing.T) {
 	// This integration test will always pass
+	t.Log("Dummy test passed")
 }
 
 func TestIntegrationDummyUpdateUser(t *testing.T) {
 	// This integration test will always pass
+	t.Log("Dummy test passed")
 }
 
 func TestIntegrationDummyDeleteUser(t *testing.T) {
 	// This integration test will always pass
+	t.Log("Dummy test passed")
 }
--- unit/unit_tests.go
+++ unit/unit_tests.go
@@ -6,16 +6,20 @@ import (
 
 func TestDummyCreateUser(t *testing.T) {
 	// This test will always pass
+	t.Log("Dummy test passed")
 }
 
 func TestDummyGetUserByEmail(t *testing.T) {
 	// This test will always pass
+	t.Log("Dummy test passed")
 }
 
 func TestDummyUpdateUser(t *testing.T) {
 	// This test will always pass
+	t.Log("Dummy test passed")
 }
 
 func TestDummyDeleteUser(t *testing.T) {
 	// This test will always pass
+	t.Log("Dummy test passed")
 }
