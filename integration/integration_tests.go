dce7fcb878bba82cf7341fef4384810b152ae9eb
Chaman Betrabet <betrabet.c@northeastern.edu>
2024-09-27 16:51:45 -0400

    Added dummy unit and integration tests for now

---
+++ integration/integration_tests.go
@@ -0,0 +1,21 @@
+package integration
+
+import (
+	"testing"
+)
+
+func TestIntegrationDummyCreateUser(t *testing.T) {
+	// This integration test will always pass
+}
+
+func TestIntegrationDummyGetUserByEmail(t *testing.T) {
+	// This integration test will always pass
+}
+
+func TestIntegrationDummyUpdateUser(t *testing.T) {
+	// This integration test will always pass
+}
+
+func TestIntegrationDummyDeleteUser(t *testing.T) {
+	// This integration test will always pass
+}
---
+++ unit/unit_tests.go
@@ -0,0 +1,21 @@
+package unit
+
+import (
+	"testing"
+)
+
+func TestDummyCreateUser(t *testing.T) {
+	// This test will always pass
+}
+
+func TestDummyGetUserByEmail(t *testing.T) {
+	// This test will always pass
+}
+
+func TestDummyUpdateUser(t *testing.T) {
+	// This test will always pass
+}
+
+func TestDummyDeleteUser(t *testing.T) {
+	// This test will always pass
+}
